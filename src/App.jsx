000b13cf4f12a7c1b16911d0829a250e8ae80951
Chamod <chamoddperera99@gmail.com>
2025-01-28 23:07:53 +0530

    QR_generater_added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Img_slider from "./components/img_slider"
 import Load_More from "./components/load_more"
 import TreeView from "./components/tree_view"
 import menus from "./components/tree_view/data"
+import QRGenerater from "./components/qrGenerater"
 
 function App() {
 
@@ -15,7 +16,8 @@ function App() {
      {/* <Star_rating noOfStars = {10}/> */}
      {/* <Img_slider url ={'https://picsum.photos/v2/list'} limit ={5}/> */}
      {/* <Load_More/> */}
-     <TreeView menus={menus}/>
+     {/* <TreeView menus={menus}/> */}
+     <QRGenerater/>
     </>
   )
 }
---
+++ src/components/qrGenerater/index.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import QRCode from 'react-qr-code';
+
+const QRGenerater = () => {
+
+  const [input,setInput] = useState('')
+
+  return (
+    <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+      <h2>QR Generater</h2>
+      <input type="text" placeholder='Enter value' onChange={(e)=>setInput(e.target.value)}/>
+      {input === '' ? null : <QRCode id='QR_value' value={input} size={400}/>}
+    </div>
+  );
+}
+
+export default QRGenerater;
